81cb9a0bc0e30020b7ebc35e020a2031f2bd703f
surajarya130 <surajarya130@gmail.com>
2024-04-23 01:00:17 +0530

    fixed: removed unnecessary file

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,8 +2,6 @@
 
 import FlightSearchForm from "@/components/FlightSearchForm";
 
-// import TestFormTwo from "@/components/TestFormTwo";
-
 const Page = () => {
   return (
     <main className="flex min-h-screen max-w-full grow  flex-col p-10">
